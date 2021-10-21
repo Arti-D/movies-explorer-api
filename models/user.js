3c4485729df98bf217c096a87024f271eb88f5ca
Arti <arti.cgd@yandex.ru>
2021-10-21 17:24:59 +0300

    feat userSchema

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: isEmail,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  { versionKey: false },
+);
+
+userSchema.static.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error('Неправильные почта или пароль'));
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return user;
+    });
+  });
+};
+
+module.exports = mongoose.model('user', userSchema);
